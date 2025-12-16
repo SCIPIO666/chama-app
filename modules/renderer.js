7b37e1baf510781084fc72e0e9fc569456f86df7
SCIPIO666 <eaphoney@gmail.com>
2025-12-16 11:09:42 +0300

    render module init

--- modules/renderer.js
+++ modules/renderer.js
@@ -1,3 +1,71 @@
 import { domElements,components} from "./domComponents.js";
 
 //add click events and render on click
+
+class Render{
+    constructor(elements,componentClass){
+        this.elements=elements;
+        this.componentClass=componentClass;
+    }
+
+    slideInNavigationOnClick(){
+
+    }
+    closeNavigationOnClick(){
+
+    }
+    renderAddMemberOnClick(){
+
+    }
+    renderMemberProfilesOnClick(){
+
+    }
+    renderContributionsOnClick(){
+
+    }
+    renderLoansOnClick(){
+
+    }
+    renderMonthlyProfitLossOnClick(){
+
+    }
+    renderProfitShareOnClick(){
+
+    }
+    renderTransactionsLedgerOnClick(){
+
+    }
+    renderNonPerformingLoansOnClick(){
+
+    }
+
+}
+
+class ModalRenderer{
+    constructor(elements,componentClass){
+        this.elements=elements;
+        this.componentClass=componentClass;
+    }
+    showModal(){
+
+    }
+    closeModal(){
+        
+    }
+    renderAddLedgerTransactionsModalOnclick(){
+
+    }
+    renderUserBorrowingHistoryModal(){
+
+    }
+    renderUserMonthlyContributionsModal(){
+
+    }
+    renderUserProfitShareModal(){
+
+    }
+    renderUserBorrowingHistoryModal(){
+
+    }
+
+}
